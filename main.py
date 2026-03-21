dff68c3346e809dde4756f4ed7139fdde71428ee
Varun Mathukumalli <varunmathukumalli@Varuns-MacBook-Pro.local>
2026-03-21 02:10:25 -0400

    Fix sys.path resolution for Railway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 
 from api import app  # noqa: E402, F401
